46c56216511ee87294fb9d5209850bdac73e757a
robelar445 <roblar757@gmail.com>
2026-02-02 08:32:52 +0100

    ..
    ..

---
+++ README.md
@@ -0,0 +1,8 @@
+# addMulApp
+
+# Demo video
+
+
+
+https://github.com/user-attachments/assets/54760d23-8650-421e-85d6-1e17e639e91a
+
---
+++ app/src/main/java/com/example/addMulApp/RandomvalueActivity.kt
@@ -0,0 +1,29 @@
+package com.example.addMulApp
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+
+class RandomvalueActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val upperLimit = intent.getIntExtra("UPPER_LIMIT", 100)
+        Log.i("RandomvalueActivity", "Upper limit = $upperLimit")
+
+
+        val randomValue = (0..upperLimit).random()
+
+
+        //Toast.makeText(this, "Tilfeldig tall: $randomValue", Toast.LENGTH_SHORT).show()
+
+        Log.i("RandomvalueActivity", "Random value = $randomValue")
+        val resultIntent = Intent()
+        resultIntent.putExtra("RANDOM_VALUE", randomValue)
+        setResult(RESULT_OK, resultIntent)
+        Log.i("RandomvalueActivity", "Upper limit = $upperLimit")
+
+        finish()
+    }
+}
